05dff2eb3fc07001ab9e0bb7d839946200f6b1e2
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:36:36 -0500

    Initial commit

---
+++ Practica_Metodos_video_11/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practica_Metodos_video_11
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(divideNumeros(18, 7));
+            Console.ReadKey(true);
+        } 
+
+
+        static double divideNumeros(double num1, int num2)=> num1 / num2;
+    }
+}
